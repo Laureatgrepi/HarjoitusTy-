f4fa3bbfecad0622cc6297ac0774e029bbd84bc4
Laureat Grepi <laureatgrepi@hotmail.com>
2018-10-16 23:09:53 +0300

    moi

---
+++ HarjoitusTyö/src/main/java/palvelinohjelmointi/HarjoitusTy/HarjoitusTyöApplication.java
@@ -0,0 +1,12 @@
+package palvelinohjelmointi.HarjoitusTy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarjoitusTyöApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarjoitusTyöApplication.class, args);
+	}
+}
